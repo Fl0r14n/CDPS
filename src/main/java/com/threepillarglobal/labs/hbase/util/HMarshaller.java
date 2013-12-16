5b82a24a717fe33856e8c52e792f4c0b492b09a9
Florian Chis <florian.chis@threepillarglobal.com>
2013-12-16 10:18:48 +0000

    HMarshaller continued testing

--- src/main/java/com/threepillarglobal/labs/hbase/util/HMarshaller.java
+++ src/main/java/com/threepillarglobal/labs/hbase/util/HMarshaller.java
@@ -2,18 +2,9 @@ package com.threepillarglobal.labs.hbase.util;
 
 import com.threepillarglobal.labs.hbase.annotation.HColumn;
 import static com.threepillarglobal.labs.hbase.util.HAnnotation.getColumnFamilyName;
-import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.math.BigDecimal;
-import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import org.apache.commons.lang.ClassUtils;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
-import org.apache.hadoop.hbase.util.Bytes;
 
 /**
  * Handless marshalling to and from HBase api
@@ -32,13 +23,13 @@ public abstract class HMarshaller {
         //get column family
         byte[] cFamily = getColumnFamilyName(clazz).getBytes();
         //create new instance
-        final T instance = instantiate(clazz);
+        final T instance = ReflectionUtil.instantiate(clazz);
         //populate with values
         for (Field field : clazz.getDeclaredFields()) {
             HColumn hColumn = field.getAnnotation(HColumn.class);
             if (hColumn != null) {
                 byte[] val = result.getValue(cFamily, hColumn.name().getBytes());
-                setFieldValue(field, instance, val);
+                ReflectionUtil.setFieldValue(field, instance, val);
             }
         }
         return instance;
@@ -59,210 +50,8 @@ public abstract class HMarshaller {
         for (Field field : clazz.getDeclaredFields()) {
             HColumn hColumn = field.getAnnotation(HColumn.class);
             if (hColumn != null) {
-                put.add(cFamily, hColumn.name().getBytes(), getFieldValue(field, obj));
+                put.add(cFamily, hColumn.name().getBytes(), ReflectionUtil.getFieldValue(field, obj));
             }
         }
-    }
-
-    /**
-     * Obtain a byte[] representation of the value inside a field
-     * @param <T> Class type
-     * @param field The Field object
-     * @param t The object instance
-     * @return field value as byte[]
-     * @throws Exception 
-     */
-    public static <T> byte[] getFieldValue(Field field, T t) throws Exception {
-        field.setAccessible(true);
-        Class<?> fieldType = field.getType();
-        switch (fieldType.getSimpleName()) {
-            case "String": {
-                return Bytes.toBytes((String) field.get(t));
-            }
-            case "boolean": {
-                return Bytes.toBytes(field.getBoolean(t));
-            }
-            case "Boolean": {
-                return Bytes.toBytes((Boolean) field.get(t));
-            }
-            case "byte": {
-                return Bytes.toBytes(field.getByte(t));
-            }
-            case "Byte": {
-                return Bytes.toBytes((Byte) field.get(t)); //do not cast to short
-            }
-            case "byte[]": {
-                return (byte[]) field.get(t);
-            }
-            case "char": {
-                return Bytes.toBytes(field.getChar(t));
-            }
-            case "Character": {
-                return Bytes.toBytes((Character) field.get(t)); //do not cast to int
-            }
-            case "short": {
-                return Bytes.toBytes(field.getShort(t));
-            }
-            case "Short": {
-                return Bytes.toBytes((Short) field.get(t));
-            }
-            case "int": {
-                return Bytes.toBytes(field.getInt(t));
-            }
-            case "Integer": {
-                return Bytes.toBytes((Integer) field.get(t));
-            }
-            case "long": {
-                return Bytes.toBytes(field.getLong(t));
-            }
-            case "Long": {
-                return Bytes.toBytes((Long) field.get(t));
-            }
-            case "float": {
-                return Bytes.toBytes(field.getFloat(t));
-            }
-            case "Float": {
-                return Bytes.toBytes((Float) field.get(t));
-            }
-            case "double": {
-                return Bytes.toBytes(field.getDouble(t));
-            }
-            case "Double": {
-                return Bytes.toBytes((Double) field.get(t));
-            }
-            case "BigDecimal": {
-                return Bytes.toBytes((BigDecimal) field.get(t));
-            }
-            case "Date": {
-                return Bytes.toBytes(((Date) field.get(t)).getTime());
-            }
-            case "Timestamp": {
-                return Bytes.toBytes(((Date) field.get(t)).getTime());
-            }
-        }
-        return null;
-    }
-
-    /**
-     * Set a Field in an object given the byte[] representation of it's value
-     * @param <T> Class type
-     * @param field The Field object
-     * @param t The object instance
-     * @param value field value to be set as byte[]
-     * @throws Exception 
-     */
-    public static <T> void setFieldValue(Field field, T t, byte[] value) throws Exception {
-        field.setAccessible(true);
-        Class<?> fieldType = field.getType();        
-        switch (fieldType.getSimpleName()) {
-            case "String": {
-                field.set(t, Bytes.toString(value));
-                return;
-            }
-            case "boolean": {
-                field.setBoolean(t, Bytes.toBoolean(value));
-                return;
-            }
-            case "Boolean": {
-                field.set(t, Bytes.toBoolean(value));
-                return;
-            }
-            case "byte": {
-                byte val = value.length>0?(byte)(value[0]&0xFF):0;
-                field.setByte(t, val);
-                return;
-            }
-            case "Byte": {
-                byte val = value.length>0?(byte)(value[0]&0xFF):0;
-                field.set(t, new Byte(val));
-                return;
-            }
-            case "byte[]": {
-                field.set(t, value);
-                return;
-            }
-            case "char": {
-                field.setChar(t, new Character((char) Bytes.toInt(value)));
-                return;
-            }
-            case "Character": {
-                field.set(t, new Character((char) Bytes.toInt(value)));
-                return;
-            }
-            case "short": {
-                field.setShort(t, Bytes.toShort(value));
-                return;
-            }
-            case "Short": {
-                field.set(t, new Short(Bytes.toShort(value)));
-                return;
-            }
-            case "int": {
-                field.setInt(t, Bytes.toInt(value));
-                return;
-            }
-            case "Integer": {
-                field.set(t, new Integer(Bytes.toInt(value)));
-                return;
-            }
-            case "long": {
-                field.setLong(t, Bytes.toLong(value));
-                return;
-            }
-            case "Long": {
-                field.set(t, new Long(Bytes.toLong(value)));
-                return;
-            }
-            case "float": {
-                field.setFloat(t, Bytes.toFloat(value));
-                return;
-            }
-            case "Float": {
-                field.set(t, new Float(Bytes.toFloat(value)));
-                return;
-            }
-            case "double": {
-                field.setDouble(t, Bytes.toDouble(value));
-                return;
-            }
-            case "Double": {
-                field.set(t, new Double(Bytes.toDouble(value)));
-                return;
-            }
-            case "BigDecimal": {
-                field.set(t, Bytes.toBigDecimal(value));
-                return;
-            }
-            case "Date": {
-                field.set(t, new Date(Bytes.toLong(value)));
-                return;
-            }
-            case "Timestamp": {
-                field.set(t, new Timestamp(Bytes.toLong(value)));
-            }
-        }
-    }
-
-    private static <T> T instantiate(Class<T> cls) throws Exception {
-        // Create instance of the given class                
-        @SuppressWarnings("unchecked")
-        final Constructor<T> constr = (Constructor<T>) cls.getConstructors()[0];
-        final List<Object> params = new ArrayList<>();
-        for (Class<?> pType : constr.getParameterTypes()) {
-            params.add((pType.isPrimitive()) ? ClassUtils.primitiveToWrapper(pType).newInstance() : null);
-        }
-        return constr.newInstance(params.toArray());
-    }
-
-    private static <T> T instantiate(Class<T> cls, Map<String, ? extends Object> args) throws Exception {
-        final T instance = instantiate(cls);
-
-        // Set separate fields
-        for (Map.Entry<String, ? extends Object> arg : args.entrySet()) {
-            Field f = cls.getDeclaredField(arg.getKey());
-            f.setAccessible(true);
-            f.set(instance, arg.getValue());
-        }
-        return instance;
     }
 }
--- src/test/java/com/threepillarglobal/labs/cpds/HMarshallerTest.java
+++ src/test/java/com/threepillarglobal/labs/cpds/HMarshallerTest.java
@@ -4,7 +4,13 @@ import com.threepillarglobal.labs.hbase.annotation.HColumn;
 import com.threepillarglobal.labs.hbase.annotation.HColumnFamily;
 import com.threepillarglobal.labs.hbase.annotation.HTable;
 import com.threepillarglobal.labs.hbase.util.HMarshaller;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
+import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import org.junit.Ignore;
 import org.junit.Test;
 
 //TODO
@@ -25,7 +31,7 @@ public class HMarshallerTest {
         public static class CFamily1 {
 
             @HColumn(name = "name")
-            String name = "Sample2";
+            String name = "Sample1";
         }
 
         @Getter
@@ -39,21 +45,38 @@ public class HMarshallerTest {
 
     @Test
     public void marshall_a_column_family_object() throws Exception {
-//        HMarshaller.marshall(this, null);
+        Put put = new Put(new byte[]{0x01});
+        Table.CFamily1 cf = new Table.CFamily1();
+        HMarshaller.marshall(cf, put);
     }
 
     @Test
     public void unmarshall_a_column_family_object() throws Exception {
-//        HMarshaller.unmarshall(Table.CFamily1.class, null);
+        List<KeyValue> kvs = new ArrayList<>();
+        {
+            kvs.add(new KeyValue("row".getBytes(), "family".getBytes(), "qualifier1".getBytes()));
+            kvs.add(new KeyValue("row".getBytes(), "family".getBytes(), "qualifier2".getBytes()));
+            kvs.add(new KeyValue("row".getBytes(), "family".getBytes(), "qualifier3".getBytes()));
+        }
+        HMarshaller.unmarshall(Table.CFamily1.class, new Result(kvs));
     }
 
     @Test
+    @Ignore
     public void marshall_a_table_object_with_all_cfamilies() throws Exception {
-//        HMarshaller.marshall(this, null);
+        Put put = new Put("row".getBytes());
+        Table table = new Table();
+        HMarshaller.marshall(table, put);
     }
 
     @Test
+    @Ignore
     public void unmarshall_a_table_object_with_all_cfamilies() throws Exception {
-//        HMarshaller.unmarshall(Table.class, null);
+        List<KeyValue> kvs = new ArrayList<>();
+        {
+            kvs.add(new KeyValue("row".getBytes(), "cfamily1".getBytes(), "Sample1".getBytes()));
+            kvs.add(new KeyValue("row".getBytes(), "cfamily2".getBytes(), "Sample2".getBytes()));
+        }
+        HMarshaller.unmarshall(Table.class, new Result(kvs));
     }
 }
