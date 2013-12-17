898a2cfa63a1f46ffed18483fa01ef508bf322d9
Cristian Paun <cristian.paun@3pillarglobal.com>
2013-12-17 14:42:18 +0200

    user service impl

--- src/main/java/com/threepillarglobal/labs/cdps/service/impl/UserServiceImpl.java
+++ src/main/java/com/threepillarglobal/labs/cdps/service/impl/UserServiceImpl.java
@@ -2,18 +2,22 @@ package com.threepillarglobal.labs.cdps.service.impl;
 
 import java.util.List;
 
+import com.threepillarglobal.labs.cdps.dao.repository.UserRepository;
 import com.threepillarglobal.labs.cdps.domain.User;
 import com.threepillarglobal.labs.cdps.service.api.UserService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service(value = "userServiceImpl")
 public class UserServiceImpl implements UserService {
-
+    
+	@Autowired
+	private UserRepository userRepo;
+	
 	@Override
 	public List<User> getUsers() {
-		// TODO Auto-generated method stub
-		return null;
+		return userRepo.findAllUser();
 	}
     
 }
