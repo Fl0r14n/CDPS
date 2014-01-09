884060e59b86079161017722e0de46a7c486787f
Florian Chis <florian.chis@threepillarglobal.com>
2014-01-09 08:52:40 +0000

    MapReduce IT not working

---
+++ src/test/java/com/threepillarglobal/labs/cpds/MapReduceIT.java
@@ -0,0 +1,70 @@
+package com.threepillarglobal.labs.cpds;
+
+import com.threepillarglobal.labs.cdps.domain.User;
+import com.threepillarglobal.labs.hbase.util.HAnnotation;
+import java.io.IOException;
+import javax.annotation.Resource;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.IdentityTableReducer;
+import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
+import org.apache.hadoop.hbase.mapreduce.TableMapper;
+import org.apache.hadoop.hbase.mapreduce.TableReducer;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.mapreduce.Job;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath*:integrationTests-context.xml")
+public class MapReduceIT {
+
+    private static final Logger L = LoggerFactory.getLogger(MapReduceIT.class);
+
+    @Resource(name = "hbaseConfiguration")
+    private Configuration config;
+
+    @Test
+    public void run_mapreduce_job() throws IOException, InterruptedException, ClassNotFoundException {
+        Job job = new Job(config, MapReduceIT.class.getName());                
+        job.setJarByClass(MapReduceIT.class);        
+        Scan scan = new Scan();
+        {
+            scan.setCaching(500);        // 1 is the default in Scan, which will be bad for MapReduce jobs
+            scan.setCacheBlocks(false);  // don't set to true for MR jobs
+            scan.addColumn("family".getBytes(), "qualifier".getBytes());
+        }        
+        TableMapReduceUtil.initTableMapperJob(HAnnotation.getTableName(User.class), scan, UserByLocationMapper.class, ImmutableBytesWritable.class, Put.class, job);
+        TableMapReduceUtil.initTableReducerJob("targetTable", UserByLocationReducer.class, job);
+        job.setNumReduceTasks(0);
+        if (!job.waitForCompletion(true)) {
+            throw new IOException("error with job!");
+        }
+    }
+
+    //key out, value out
+    static class UserByLocationMapper extends TableMapper<ImmutableBytesWritable, Put> {
+
+        @Override
+        protected void map(ImmutableBytesWritable key, Result value, Context context) throws IOException, InterruptedException {
+            System.out.println("Mapper key:="+Bytes.toString(key.get()));
+        }
+    }
+
+    //key in, value in, key out
+    static class UserByLocationReducer extends TableReducer<ImmutableBytesWritable, Put, ImmutableBytesWritable> {
+
+        @Override
+        protected void reduce(ImmutableBytesWritable key, Iterable<Put> values, Context context) throws IOException, InterruptedException {
+            //TODO
+        }
+    }
+}
